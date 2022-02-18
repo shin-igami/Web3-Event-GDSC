b8d9966b6a37e087349b4236586011d3978333f2
Manav Agarwal <manavagarwal1410@gmail.com>
2022-02-18 17:38:23 +0530

    Customized Speaker's section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "./Button";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
 import { motion } from "framer-motion";
 import "../styles/components/Blog.scss";
 
@@ -15,17 +15,26 @@ function Blog({ image, title, subTitle, variants, animate }) {
         <img src={image} alt="Blog" />
       </div>
       <div className="content">
-        <div className="title">
-          <h3>{title}</h3>
+        <div className="content-text">
+          <div className="title">
+            <h3>{title}</h3>
+          </div>
+          <div className="subTitle">
+            <p>{subTitle}</p>
+          </div>
         </div>
-        <div className="subTitle">
-          <p>{subTitle}</p>
+        <div className="socialButtons">
+          <Button
+            // content="Read more"
+            color="inverse-no_outline"
+            icon={<FaLinkedinIn />}
+            />
+          <Button
+            // content="Read more"
+            color="inverse-no_outline"
+            icon={<FaFacebookF />}
+            />
         </div>
-        <Button
-          content="Read more"
-          color="inverse"
-          icon={<HiOutlineArrowNarrowRight />}
-        />
       </div>
     </motion.div>
   );
