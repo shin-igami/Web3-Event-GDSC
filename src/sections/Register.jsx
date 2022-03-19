497135e35616068014666843aade80ed39634b9b
Mohsin <razam305@gmail.com>
2022-03-19 16:12:08 +0530

    About and register

---
+++ src/sections/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/sections/Register.css'
+const Register = () => {
+  return (
+    <div className='register'>
+        <div className="register-content">
+            <h1>Are you Ready to attend  Web3 Summit ?</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat nisl pretium fusce id velit. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Nulla pellentesque dignissim enim sit.</p>
+        </div>
+        <div className="register-button">
+            <a href=""><span>Register Now</span></a>
+        </div>
+    </div>
+  )
+}
+
+export default Register
