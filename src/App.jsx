136a144a2062f47e7588fa00b50f2fba9dc9433c
Shailesh Tiwari <iib2020001@iiita.ac.in>
2022-03-21 15:08:41 +0530

    added prettier file for smooth linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import React from "react";
-import Testimonials from "./sections/Testimonials";
-import About from "./sections/About";
-import Footer from "./sections/Footer";
-import Starter from "./sections/Starter";
+import React from 'react';
+import Testimonials from './sections/Testimonials';
+import About from './sections/About';
+import Footer from './sections/Footer';
+import Starter from './sections/Starter';
 // import Why from "./sections/Why";
-import Blogs from "./sections/Blogs";
-import ScrollToTop from "./components/ScrollToTop";
-import { motion } from "framer-motion";
-import { withRoot } from "./withRoot";
-import Register from "./sections/Register";
-import Schedule from "./sections/Schedule";
+import Blogs from './sections/Blogs';
+import ScrollToTop from './components/ScrollToTop';
+import { motion } from 'framer-motion';
+import { withRoot } from './withRoot';
+import Register from './sections/Register';
+import Schedule from './sections/Schedule';
 
 function App() {
-	return (
-		<motion.div initial="hidden" animate="show">
-			<ScrollToTop />
-			<Starter />
-			<About />
-			<Schedule />
-			<Register />
-			{/* <Why /> */}
-			<Testimonials />
-			<Blogs />
-			<Footer />
-		</motion.div>
-	);
+    return (
+        <motion.div initial="hidden" animate="show">
+            <ScrollToTop />
+            <Starter />
+            <About />
+            <Schedule />
+            <Register />
+            {/* <Why /> */}
+            <Testimonials />
+            <Blogs />
+            <Footer />
+        </motion.div>
+    );
 }
-
 export default App;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import "../styles/components/Title.scss";
+import React from 'react';
+import '../styles/components/Title.scss';
 
 function Title({ title, color, lineCenter = false }) {
   return (
-    <div className={`title ${lineCenter === true ? "center" : ""}`}>
+    <div className={`title ${lineCenter === true ? 'center' : ''}`}>
       <h2>{title}</h2>
       <div className={`${color} `}>
         <span></span>
